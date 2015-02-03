c8effb0155cdd8fd972b1f6f20d2d2b871f0a46a
Christopher Findeisen <cfindeisen7@gmail.com>
2015-02-02 18:44:39 -0600

    Linked List implementation (buggy)

--- src/part1/linked_list.c
+++ src/part1/linked_list.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
+#include <string.h>
 #include "linked_list.h"
+/* #define _1_INT_OFFSET_ sizeof(int) */
+/* #define _2_INT_OFFSET_ sizeof(int)*2 */
+static char* head_ptr;
+static void* free_ptr;
+static int memory_pool;
+static int block_size;
 
-void 	Init (int M, int b){} 
-void 	Destroy (){} 		 
-int 	Insert (int key,char * value_ptr, int value_len){}
-int 	Delete (int key){}
-char* 	Lookup (int key){return NULL;}
-void 	PrintList (){}
+void  Init (int M, int b){
+  head_ptr = (char*)malloc(M);
+  free_ptr = head_ptr;
+  memory_pool = M;
+  block_size = b;
+}
+void  Destroy (){
+  free(head_ptr);
+}
+int   Insert (int key,char* value_ptr, int value_len){
+  if(block_size - ptr_offset == value_len)
+  {
+      //throw
+  }
+  *(int*)(free_ptr + key_offset) = key;
+  *(int*)(free_ptr + len_offset) = value_len;
+  memcpy(free_ptr + ptr_offset, value_ptr, value_len);
+  free_ptr += block_size;
+  //TODO:find out spec meaning
+  return key;
+}
+int   Delete (int key){
+  void* delete_ptr = head_ptr;
+  int i;
+
+  for(i = 0; i < memory_pool/b; ++i) {
+    if(*(int*)delete_ptr != key) {
+      delete_ptr += block_size;
+    }
+    else {
+      (free_ptr + key_offset) = NULL;
+      (free_ptr + len_offset) = NULL;
+      (delete_ptr + ptr_offset) = NULL;
+      break;
+    }
+  }
+
+}
+
+char*   Lookup (int key){return NULL;}
+void  PrintList (){}
