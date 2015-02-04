0cf5accbd8d4a7fb634452a4b5768fa3a86c9ee5
Christopher Joseph Findeisen <cfin7@build.tamu.edu>
2015-02-04 10:48:19 -0600

    Fixed a few bugs..identified prob as insert(13)

--- src/part1/linked_list.c
+++ src/part1/linked_list.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "linked_list.h"
 /* #define _1_INT_OFFSET_ sizeof(int) */
 /* #define _2_INT_OFFSET_ sizeof(int)*2 */
-static char* head_ptr;
+static void* head_ptr;
 static void* free_ptr;
 static int memory_pool;
 static int block_size;
 
 void  Init (int M, int b){
-  head_ptr = (char*)malloc(M);
+  //calloc gets rid of free() problem, but introduces other one.
+  //The real problem is insert 13 where they use a null byte
+  head_ptr = malloc(M);
+
   free_ptr = head_ptr;
   memory_pool = M;
   block_size = b;
@@ -28,30 +32,33 @@ int   Insert (int key,char* value_ptr, int value_len){
   }
   else {
     //set previous nodes "next" ptr
-    *(char*)(free_ptr + block_size) = free_ptr;
+    if(free_ptr != head_ptr) {
+      *(char*)(free_ptr - block_size) = free_ptr;
+    }
     *(int*)(free_ptr + key_offset) = key;
     *(int*)(free_ptr + len_offset) = value_len;
     memcpy(free_ptr + ptr_offset, value_ptr, value_len);
-    /* printf("STRING INSERT %s \n", value_ptr); */
+    
     free_ptr += block_size;
   }
-  //TODO:find out spec meaning
+  //TODO:find out spec meaning(what do we return
   return 1;
 }
 int   Delete (int key){
   void* ptr = head_ptr;
-
-  for(int i = 0; i <= memory_pool/block_size; ++i) {
+  int i;
+  for(i = 0; i <= memory_pool/block_size; ++i) {
     if(getNodeKey(ptr) != key) {
       ptr += block_size;
     }
     else {
-      /* printf("trying to delete this %d \n", getNodeKey(ptr)); */
-      *(char*)(free_ptr - block_size) = (free_ptr + block_size);
-      *(char*)(ptr) = NULL;
+      if(ptr!=head_ptr) {
+        *(char*)(ptr - block_size) = (ptr + block_size);
+      }
       *(int*)(ptr + key_offset) = NULL;
       *(int*)(ptr + len_offset) = NULL;
-      *(char*)(ptr + ptr_offset) = NULL;
+      *(char*)(ptr + ptr_offset) = '\0';
+      *(char*)(ptr) = '\0';
       return 1;
     }
   }
@@ -60,28 +67,32 @@ int   Delete (int key){
 
 char* Lookup (int key) {
 
-  void* ptr = head_ptr;
-  for(int i = 0; i <= memory_pool/block_size; ++i) {
+  char* ptr = head_ptr;
+  int i;
+  for(i = 0; i <= memory_pool/block_size; ++i) {
     if(getNodeKey(ptr) == key && key != NULL) {
-      return (char*)ptr + key_offset;
+      return (ptr + key_offset);
     }
     else {
       ptr += block_size;
     }
   }
-  return NULL;
+  return '\0';
 }
 
 void PrintString( int num_of_chars, char* to_print ) {
-  for(int i = 0; i < num_of_chars; ++i) {
+  int i;
+  for(i = 0; i < num_of_chars; ++i) {
     printf("%c",*(++to_print));
   }
 }
 
 void  PrintList () {
   void * ptr = head_ptr;
-  for(int i = 0; i <= memory_pool/block_size; ++i) {
-    if(getNodeKey(ptr) != NULL) {
+  int i;
+//getNodeKey(ptr) == key && key != NULL
+  for(i = 0; i <= memory_pool/block_size; ++i) {
+    if(getNodeKey(ptr) != NULL && getNodeValueLength(ptr) != NULL) {
       printf("Key: %d , ValueLength: %d \n", getNodeKey(ptr), getNodeValueLength(ptr));
     }
       ptr += block_size;
